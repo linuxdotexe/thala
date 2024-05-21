fbaba88107b45b9a7c063d233de2bc77753fb923
sai nivas mangu <sainonbeat99@gmail.com>
2024-05-21 10:56:54 +0530

    upd: logic viewer UI

--- src/components/LogicViewer.tsx
+++ src/components/LogicViewer.tsx
@@ -3,37 +3,54 @@ import { useEffect, useState } from "react";
 const textLoadTime = 1300;
 const scrollToThala = 300;
 
-export default function LogicViewer({ elements, text, trigger }: { elements: Array<JSX.Element>, text: string, trigger: boolean }) {
-    const [visibleCount, setVisibleCount] = useState(0);
+export default function LogicViewer({
+  elements,
+  text,
+  trigger,
+}: {
+  elements: Array<JSX.Element>;
+  text: string;
+  trigger: boolean;
+}) {
+  const [visibleCount, setVisibleCount] = useState(0);
 
-    useEffect(() => {
-        if (visibleCount < elements.length) {
-            const timer = setTimeout(() => {
-                setVisibleCount(visibleCount + 1);
-            }, textLoadTime);
-            let timer2: NodeJS.Timeout;
-            if (visibleCount === elements.length - 1) {
-                const timer2 = setTimeout(() => {
-                    document.getElementById("thala-4-a-reason")?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-                }, textLoadTime + scrollToThala);
-            }
-            return () => {
-                // console.log("cleanup internal");
-                clearTimeout(timer);
-                timer2 ? clearTimeout(timer2) : null; 
-            }
-        }
-    }, [visibleCount, elements.length]);
+  useEffect(() => {
+    if (visibleCount < elements.length) {
+      const timer = setTimeout(() => {
+        setVisibleCount(visibleCount + 1);
+      }, textLoadTime);
+      let timer2: NodeJS.Timeout;
+      if (visibleCount === elements.length - 1) {
+        const timer2 = setTimeout(() => {
+          document.getElementById("thala-4-a-reason")?.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest",
+          });
+        }, textLoadTime + scrollToThala);
+      }
+      return () => {
+        // console.log("cleanup internal");
+        clearTimeout(timer);
+        timer2 ? clearTimeout(timer2) : null;
+      };
+    }
+  }, [visibleCount, elements.length]);
 
-    useEffect(() => {
-        setVisibleCount(0);
-    }, [trigger]);
+  useEffect(() => {
+    setVisibleCount(0);
+  }, [trigger]);
 
-    return (
-        <div className="flex flex-col items-center justify-center text-center text-2xl m-5">
-            {elements.slice(0, visibleCount).map((element, index) => (
-                <div key={index + "-logic"} className="m-1">{element}</div>
-            ))}
+  return (
+    <div
+      className="flex flex-col items-center justify-center text-md
+      m-12 p-4 bg-gradient-to-bl from-neutral-800 to-neutral-900
+      rounded-md">
+      {elements.slice(0, visibleCount).map((element, index) => (
+        <div key={index + "-logic"} className="">
+          {element}
         </div>
-    );
-};
+      ))}
+    </div>
+  );
+}
--- src/components/Trick.tsx
+++ src/components/Trick.tsx
@@ -3,53 +3,52 @@ import PreTricks from "@/pre-tricks/PreTricks";
 import LogicViewer from "@/components/LogicViewer";
 
 function validSubSeq(text: string) {
-    const dhoni = "dhoni";
-    text = text.toLowerCase();
-    let k = 0;
-    for (let i = 0; i < text.length; i++) {
-        if (dhoni[k] === text[i]) {
-            k++;
-        }
-        if (k === dhoni.length) {
-            return true;
-        }
+  const dhoni = "dhoni";
+  text = text.toLowerCase();
+  let k = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (dhoni[k] === text[i]) {
+      k++;
     }
-    return false;
+    if (k === dhoni.length) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function pickATrick(text: string) {
-    const validTricks: Array<(text: string) => JSX.Element[]> = [];
-    if (validSubSeq(text)) {
-        validTricks.push(PreTricks.SubSeq);
-    } else {
-        // this else should be removed for more randomness
-        const randomIndex = Math.round(Math.random() * (Tricks.length - 1));
-        validTricks.push(Tricks[randomIndex]);
-    }
+  const validTricks: Array<(text: string) => JSX.Element[]> = [];
+  if (validSubSeq(text)) {
+    validTricks.push(PreTricks.SubSeq);
+  } else {
+    // this else should be removed for more randomness
+    const randomIndex = Math.round(Math.random() * (Tricks.length - 1));
+    validTricks.push(Tricks[randomIndex]);
+  }
 
-    return validTricks;
+  return validTricks;
 }
 
-export default function Trick({ text, trigger }: { text: string, trigger: boolean }) {
-    const validTricks = pickATrick(text);
-    const randomIndex = Math.round(Math.random() * (validTricks.length - 1));
-    const currentTrick = validTricks[randomIndex];
-    const elements = currentTrick(text);
-    elements.push(
-        <h3
-            id="thala-4-a-reason"
-            className="text-5xl
+export default function Trick({
+  text,
+  trigger,
+}: {
+  text: string;
+  trigger: boolean;
+}) {
+  const validTricks = pickATrick(text);
+  const randomIndex = Math.round(Math.random() * (validTricks.length - 1));
+  const currentTrick = validTricks[randomIndex];
+  const elements = currentTrick(text);
+  elements.push(
+    <h3
+      id="thala-4-a-reason"
+      className="text-3xl
             italic font-bold
-            text-yellow-400"
-        >
-            Thala for a reason!
-        </h3>
-    );
-    return (
-        <LogicViewer
-            elements={elements}
-            text={text}
-            trigger={trigger}
-        />
-    );
+            text-amber-200">
+      Thala for a reason!
+    </h3>
+  );
+  return <LogicViewer elements={elements} text={text} trigger={trigger} />;
 }
